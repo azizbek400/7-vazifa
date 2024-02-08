6f1e24f02c7b51c42ac3cc719737456909e8f175
azizbek400 <aziznormurodov400@gmail.com>
2024-02-08 08:11:00 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,122 @@
+// 5-masala       //////////////////////////////
+
+// const str = ['azizbek', 'Nurmurodov'];
+// str.reverse();
+// console.log(str);
+
+
+
+// 1-masala       ////////////////////////////////
+
+// function number(num) {
+//     let str = num.toString();
+//     let rooms = str.length;
+//     let sum = 0;
+//     for (let i of str) {
+//         sum += +i;
+//     }
+//     return {
+//         rooms,
+//         sum,
+//     }
+// }
+
+// console.log(number(35679));
+
+
+// 15-masala   /////////////////////////////////////////
+
+// let str = 'Hello world';
+// let world = str.split('');
+
+// console.log(world);
+
+
+// 1-masala     ////////////////////////////////////
+
+// for (let i = 1; i <= 26; i++) {
+//     if (1 <= i <= 26) {
+//         console.log(true);
+//     } else {
+//         console.log(false);
+//     }
+    
+// }
+
+
+// 7-masala    ////////////////////////////////////
+
+// function number(num) {
+//         let str = num.toString();
+//         let people = 0;
+//         for (let i of str) {
+//             people += +i;
+//         }
+//         return {
+//             people,
+//         }
+//     }
+    
+//     console.log(number(405239));
+
+
+// 8-masala        ////////////////////////////////
+
+// let str = "Men 10-oktyabrda tug'ilganman";
+// let arr = [...str];
+
+// console.log(arr);
+
+
+// 9-masala       //////////////////////////////////
+
+// let str = "WHAT IS YOUR NAME";
+// console.log(str);
+// let lower = str.toLowerCase()
+
+// console.log(lower);
+
+
+// 10-masala        /////////////////////////////////////
+
+// let str = "do you work";
+// console.log(str);
+// let upper = str.toUpperCase()
+
+// console.log(upper);
+
+
+// 3-masala    /////////////////////////////////////
+
+// let letter = 'SCHOOLSCHOOLLUTF'
+// let str = letter.slice(-3);
+// let letter1 = str.padStart(16, '*')
+
+// console.log(letter1);
+
+
+// 15-masala            ////////////////////////////////////
+
+// let str = 'Uzbekistan is a great country';
+// let letter = str.split('');
+
+// console.log(letter);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
